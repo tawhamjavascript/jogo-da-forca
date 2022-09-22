c8a1abce1754b11c4bad518b6654f8dc7754e97a
tawhamjavascript <taw.ham.balbino9914@gmail.com>
2022-09-22 09:19:07 -0300

    funções iniciar e getPosições

---
+++ src/Forca.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.lang.String;
+import java.util.Random;
+
+public class Forca {
+    private ArrayList<String> palavras = new ArrayList<>(); // lista de palavras lidas do arquivo
+    private ArrayList<String> dicas = new ArrayList<>(); // lista de dicas lidas do arquivo
+    private String dica= ""; // dica da palavra sorteada
+    private String[] letras; // letras da palavra sorteada
+    private int acertos; // contador de acertos
+    private int penalidade;
+    private final ArrayList<String> letrasJogadas = new ArrayList<>();
+
+    public void iniciar () {
+        Random random = new Random();
+        int temp = random.nextInt(palavras.size());
+        letras = palavras.get(temp).split("");
+        dica = dicas.get(temp);
+
+
+
+    }
+
+    public ArrayList<Integer> getPosicoes(String letra) throws Exception {
+        ArrayList<Integer> temp = new ArrayList<>();
+        if (letrasJogadas.contains(letra)) {
+            throw new Exception("Letra já jogada");
+        }
+        else if (letra.length() < 2 && Character.isLetter(letra.charAt(0))) {
+            for(int i = 0; i < letras.length; i++) {
+                if (letras[i].equals(letra)) {
+                    temp.add(i);
+                    letras[i] = "*";
+                    acertos += 1;
+
+                }
+            }
+            letrasJogadas.add(letra);
+            if (temp.size() == 0) penalidade += 1;
+            return temp;
+        }
+        else {
+            throw new Exception("Entrada inválida");
+        }
+    }
+}
